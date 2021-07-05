54be059adf2de5349318a8a4e6a370718289de83
Santiago Gonzalez <40776497+SantiGO055@users.noreply.github.com>
2021-07-05 09:04:28 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  apiKey: "AIzaSyD8JloN3a4T1lUUDZxEbaYTD3wp45E40jE",
+    authDomain: "saladejuegos-7da4f.firebaseapp.com",
+    databaseURL: "https://saladejuegos-7da4f-default-rtdb.firebaseio.com",
+    projectId: "saladejuegos-7da4f",
+    storageBucket: "saladejuegos-7da4f.appspot.com",
+    messagingSenderId: "526051580561",
+    appId: "1:526051580561:web:be8676f61e3d6efd40bd30",
+    measurementId: "G-N24ESHKJ34"
+
 };
 
 /*
